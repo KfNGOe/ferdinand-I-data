5088883b5dd6f8913ef140acd76ae479b0657b93
rh <richard.hoermann@inode.at>
2023-06-21 11:47:18 +0200

    update

--- ghactions-docx2dtabf_id_all.sh
+++ ghactions-docx2dtabf_id_all.sh
@@ -8,6 +8,7 @@ inputDir="data/docx/band_001/"
 teiOutputDir="data/tei/band_001/"
 dtabfOutputDir="data/dtabf/band_001/"
 dtabfIdOutputDir="data/dtabf_id/band_001/"
+inputDirJson="data/json/"
 
 filesChanged=false;
 
@@ -15,58 +16,79 @@ cwd=$(pwd)
 
 mkdir -p ${teiOutputDir}temp
 
-for changed_file in $changes; do
-  echo "Found changed file: ${changed_file}."
-  if [[ "$changed_file" == *"$inputDir"* ]]; then
-    #check if file is docx
-    if [[ "$changed_file" == *".docx" ]]; then
-      echo "Found changed docx: ${changed_file}"
-      filesChanged=true;
-      name=$(basename "$changed_file" .docx)
-      echo $name
-    fi
-
-    if test -f "$outputDir$name.xml"; then
-      echo "removing old xml file"
-      rm "$outputDir$name.xml"
-    fi
-
-    if test -f "$changed_file"; then
-      echo "Docx was changed/added. Starting transform"
-      echo "Starting docx to tei transformation"
+if [ -z "$changes" ]
+then
+      echo "\$changes is empty"
+else
+      echo "\$changes is NOT empty"
       
-      echo "common part"
-      ant -f src/docx2tei/docx/build-from.xml -DinputFile=../../../$changed_file -DoutputFile="${cwd}/${teiOutputDir}temp/$name.xml"
-      
-      echo "custom part"
-      java -cp src/docx2tei/saxon-he-10.jar net.sf.saxon.Transform -s:"${cwd}/${teiOutputDir}temp/$name.xml" -xsl:"${cwd}/src/docx2tei/docx2tei.xsl" -o:"${cwd}/${teiOutputDir}$name.xml"
-  
-      echo "Starting tei to dtabf transformation"
-      java -cp src/tei2dtabf/xquery/saxon-he-10.jar net.sf.saxon.Query src/tei2dtabf/xquery/TeiToDtaBf.xquery -s:$teiOutputDir$name.xml -o:$dtabfOutputDir$name.xml
-    fi
-  fi
-done
+      if [[ "$changes" == *"$inputDirJson"* ]]; 
 
-rm -r ${teiOutputDir}temp
+      then
+        echo "Found changed json: ${changes}"
+        filesChanged=true;
+        echo transforming dtabf to dtabf_id
+        bash dtabf2dtabf_id_all.sh 
+        
+      else echo "no json changed"
+           echo "checking docx files"    
 
-if [ "$filesChanged" = true ]; then
-  echo cleaning up files $dtabfOutputDir
-  #loop over every file in docx folder
-  for file in $dtabfOutputDir*; do
-    name=$(basename "$file" .xml)
-    #check if name isnt occuring in docx folder
-    echo checking $file
-    if ! test -f "$inputDir$name.docx"; then
-    #check if file exists
-      if test -f "$file"; then
-        echo "removing $file"
-        rm "$file"
-      fi
-    fi
-  done
-  echo transforming dtabf to dtabf_id
-  bash dtabf2dtabf_id_all.sh
+          for changed_file in $changes; do
+              echo "Found changed file: ${changed_file}."
+              if [[ "$changed_file" == *"$inputDir"* ]]; then
+                #check if file is docx
+                if [[ "$changed_file" == *".docx" ]]; then
+                  echo "Found changed docx: ${changed_file}"
+                  filesChanged=true;
+                  name=$(basename "$changed_file" .docx)
+                  echo $name
+                fi
+
+                if test -f "$outputDir$name.xml"; then
+                  echo "removing old xml file"
+                  rm "$outputDir$name.xml"
+                fi
+
+                if test -f "$changed_file"; then
+                  echo "Docx was changed/added. Starting transform"
+                  echo "Starting docx to tei transformation"
+                  
+                  echo "common part"
+                  ant -f src/docx2tei/docx/build-from.xml -DinputFile=../../../$changed_file -DoutputFile="${cwd}/${teiOutputDir}temp/$name.xml"
+                  
+                  echo "custom part"
+                  java -cp src/docx2tei/saxon-he-10.jar net.sf.saxon.Transform -s:"${cwd}/${teiOutputDir}temp/$name.xml" -xsl:"${cwd}/src/docx2tei/docx2tei.xsl" -o:"${cwd}/${teiOutputDir}$name.xml"
+              
+                  echo "Starting tei to dtabf transformation"
+                  java -cp src/tei2dtabf/xquery/saxon-he-10.jar net.sf.saxon.Query src/tei2dtabf/xquery/TeiToDtaBf.xquery -s:$teiOutputDir$name.xml -o:$dtabfOutputDir$name.xml
+                fi
+              fi
+          done
+
+          rm -r ${teiOutputDir}temp
+
+          if [ "$filesChanged" = true ]; then
+            echo cleaning up files $dtabfOutputDir
+            #loop over every file in docx folder
+            for file in $dtabfOutputDir*; do
+              name=$(basename "$file" .xml)
+              #check if name isnt occuring in docx folder
+              echo checking $file
+              if ! test -f "$inputDir$name.docx"; then
+              #check if file exists
+                if test -f "$file"; then
+                  echo "removing $file"
+                  rm "$file"
+                fi
+              fi
+            done
+            echo transforming dtabf to dtabf_id
+            bash dtabf2dtabf_id_all.sh
+          fi
+      fi      
 fi
+
+
 
 echo transformation was successfull
 
